fad9fc6121b6698623acc143f7adef02cc80791d
hbn <3486175031@qq.com>
2024-11-17 21:51:15 +0800

    classification-finished

--- python/AiIntro/classification/bank-knn.py
+++ python/AiIntro/classification/bank-knn.py
@@ -9,56 +9,35 @@ import matplotlib.pyplot as plt
 from pylab import mpl
 mpl.rcParams['font.sans-serif'] = ['SimHei']
 # 加载数据集
-file_path = 'C:\\Users\\hbn\\Desktop\\Bank\\bank-additional.csv'
+file_path = 'C:\\Users\\hbn\\Desktop\\AiIntro\\Bank\\bank-additional.csv'
 data = pd.read_csv(file_path, sep=';')
 
-# 分离特征和标签
+
 X = data.drop(columns=['y'])
 y = data['y']
-
-# 实例化LabelEncoder
 label_encoder = LabelEncoder()
-
-# 对标签进行编码
 y = label_encoder.fit_transform(y)
-
-# 识别分类特征和数值特征
 categorical_features = X.columns[X.dtypes == 'object'].tolist()
 numerical_features = X.columns[X.dtypes != 'object'].tolist()
-
-# 对分类特征进行OneHot编码，数值特征保持不变
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', 'passthrough', numerical_features),
         ('cat', OneHotEncoder(), categorical_features)
     ])
 
-# 应用预处理器到特征数据集
+
 X_processed = preprocessor.fit_transform(X)
-
-# 分割数据集为训练集和测试集
 X_train, X_test, y_train, y_test = train_test_split(X_processed, y, test_size=0.3, random_state=42)
-
-# 标准化特征值
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# 初始化KNN分类器
+
 knn = KNeighborsClassifier(n_neighbors=5)
-
-# 训练模型
 knn.fit(X_train, y_train)
-
-# 预测测试集
 y_pred = knn.predict(X_test)
-
-# 计算准确率
 accuracy = accuracy_score(y_test, y_pred)
 print(f'预测准确率: {accuracy:.2f}')
-
-
-# 测试不同K值的准确率
 neighbors = list(range(1, 11))
 accuracies = []
 
--- python/AiIntro/classification/iris-nbc.py
+++ python/AiIntro/classification/iris-nbc.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
 # 加载数据集
-file_path = 'C:\\Users\\hbn\\Desktop\\Iris\\iris.csv'
+file_path = 'C:\\Users\\hbn\\Desktop\\AiIntro\\Iris\\iris.csv'
 data = pd.read_csv(file_path, header=None)
 
-# 显示前几行数据以检查
 print(data.head())
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
@@ -27,13 +26,9 @@ X_test = scaler.transform(X_test)
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
-# 初始化朴素贝叶斯分类器
+
 nb_classifier = GaussianNB()
-
-# 训练模型
 nb_classifier.fit(X_train, y_train)
-
-# 预测测试集
 y_pred = nb_classifier.predict(X_test)
 
 # 计算准确率
